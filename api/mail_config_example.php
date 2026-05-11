e4be5b143eaa93c35c85d5d9f5dfa7b2748a7440
2**10 <aaron911024@gmail.com>
2026-05-11 08:44:09 +0800

    v0.0.2 - 2026/05/11 - Modify： 1. 刪除敏感資訊。

--- api/mail_config_example.php
+++ api/mail_config_example.php
@@ -7,8 +7,8 @@ define('MAIL_PORT', 587);
 define('MAIL_ENCRYPTION', 'tls'); // 或者使用 'ssl' (Port 465)
 
 // 寄件者帳號資訊
-define('MAIL_USERNAME', 'chatbotnsysucse@gmail.com');      // TODO: 填入你的 Gmail
-define('MAIL_PASSWORD', 'tjnv bzpn ktyk dofa');   // TODO: 填入 16 位數應用程式密碼
+define('MAIL_USERNAME', '');      // TODO: 填入你的 Gmail
+define('MAIL_PASSWORD', '');   // TODO: 填入 16 位數應用程式密碼
 define('MAIL_FROM_NAME', '圖書館系統');          // 寄件者顯示名稱
 
 // 字體設定
